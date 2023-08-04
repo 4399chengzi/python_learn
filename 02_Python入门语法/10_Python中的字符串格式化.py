68d9d20341ffb8e51808432d8e858a023915896a
4399chengzi <981691590@qq.com>
2023-08-04 21:33:57 +0800

    03_Python中的字符串格式化.py

---
+++ 02_Python入门语法/10_Python中的字符串格式化.py
@@ -0,0 +1,16 @@
+# 通过占位的形式，完成拼接
+name = "周鹏程"
+message = "学IT来：%s" % name
+print(message)
+
+# 通过占位的形式，完成数字和字符串的拼接
+class_num = 57
+avg_salary = 16781
+message = "第%s期,平均工资%s元" % (class_num, avg_salary)
+print(message)
+
+name = "传智播客"
+setup_year = 2006
+stock_price = 19.99
+message = "%s%d%f" % (name, setup_year, stock_price)
+print(message)
