c83857182c8f1957d7f448312f4399f04f7dd6af
4399chengzi <981691590@qq.com>
2023-08-04 20:55:00 +0800

    03_Python中的标识符.py

---
+++ 02_Python入门语法/06_Python中的标识符.py
@@ -0,0 +1,16 @@
+# 规则1：内容限定，限定只能使用：中文，英文，数字，下划线，注意：不能以数字开头
+# 1_name = "张三"
+# name_! = "张三"
+name_1 = "张三"
+
+# 规则2：大小写敏感
+Itheim = "黑马程序员"
+itheim = "666"
+print(Itheim)
+print(itheim)
+
+# 规则3：不可使用关键字
+# class = 1
+# def = 1
+Class = 1
+print(1)
