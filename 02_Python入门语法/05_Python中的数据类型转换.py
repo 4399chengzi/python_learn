643d2633d9c7cacfd3c8ef137274474c3021d1a9
4399chengzi <981691590@qq.com>
2023-08-04 20:46:19 +0800

    03_Python中的数据类型转换.py

---
+++ 02_Python入门语法/05_Python中的数据类型转换.py
@@ -0,0 +1,24 @@
+# 将数字类型转换成字符串
+num_str = str(111)
+print(type(num_str), num_str)
+
+float_str = str(111.345)
+print(type(float_str), float_str)
+
+# 将字符串转换成数字
+num = int("111")
+print(type(num), num)
+
+num2 = float("111.345")
+print(type(num2), num2)
+
+# num3 = int("黑马程序员")
+# print(type(num3), num3)  # 失败
+
+# 整数转浮点数
+float_num = float(111)
+print(type(float_num), float_num)  # 111.0
+
+# 浮点数转整数
+int_num = int(111.345)
+print(type(int_num), int_num)  # 111
