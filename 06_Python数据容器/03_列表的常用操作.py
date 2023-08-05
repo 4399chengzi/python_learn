d7388d1d86ddbad637c3f46cf3d27fcf70f50a3f
4399chengzi <981691590@qq.com>
2023-08-05 23:27:49 +0800

    列表的常用操作（值得一看，列表万金油）

---
+++ 06_Python数据容器/03_列表的常用操作.py
@@ -0,0 +1,43 @@
+"""
+列表的常用操作
+多个，可重复，可修改，有序，不同类型
+"""
+
+my_list = ['z', 'p', 'c']
+
+# index = my_list.index('z')
+# print(index)
+#
+# # index = my_list.index('s')
+# # print(index)
+#
+# my_list[0] = 1
+# print(my_list)
+#
+# my_list[-3] = 2
+# print(my_list)
+#
+# my_list.insert(1, 'z')
+# print(my_list)
+#
+# my_list.append('s')
+# print(my_list)
+#
+# my_list.extend([1, 2, 3])
+# print(my_list)
+
+# del my_list[2]
+# print(my_list)
+#
+# my_list.pop(1)
+# print(my_list)
+#
+# my_list.remove('z')
+# print(my_list)
+
+# my_list.clear()
+# print(my_list)
+
+print(my_list.count('z'))
+
+print(len(my_list))
