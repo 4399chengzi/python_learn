2a243da07013d1a45010c9645f1dc7b99daeac9b
4399chengzi <981691590@qq.com>
2023-08-05 23:14:03 +0800

    列表定义语法

---
+++ 06_Python数据容器/01_列表定义语法.py
@@ -0,0 +1,15 @@
+"""
+列表定义语法
+"""
+
+name_list = ['z', 'p', 'c']
+print(name_list)
+print(type(name_list))
+
+my_list = ['z', 1, True]
+print(my_list)
+print(type(my_list))
+
+my_list = [[1, 2, 3], [4, 5, 6]]
+print(my_list)
+print(type(my_list))
