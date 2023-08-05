7978ef8cf4cc0466111c0c508b9fdda7e0c41314
4399chengzi <981691590@qq.com>
2023-08-05 11:36:58 +0800

    布尔类型和比较运算符

---
+++ 03_Python判断语句/01_布尔类型和比较运算符.py
@@ -0,0 +1,25 @@
+"""
+演示布尔类型的定义
+以及比较运算符的运用
+"""
+# 定义变量存储布尔类型的数据
+# 比较运算符的使用
+# ==,!=,>,<,>=,<=
+# 演示进行内容的相等比较
+num1 = 10
+num2 = 15
+print(f"{num1 != num2}")
+
+name1 = "zpc"
+name2 = "zp"
+print(f"{name1 == name2}")
+
+# 演示大于小于，大于等于小于等于的比较运算
+num1 = 10
+num2 = 10
+print(f"{num1 >= num2}")
+print(f"{num1 <= num2}")
+num2 = 11
+print(f"{num1 >= num2}")
+print(f"{num1 <= num2}")
+
