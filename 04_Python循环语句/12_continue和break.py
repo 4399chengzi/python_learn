1b753955ffc61daaeb7394f9485f531a99676e86
4399chengzi <981691590@qq.com>
2023-08-05 21:34:47 +0800

    continue和break

---
+++ 04_Python循环语句/12_continue和break.py
@@ -0,0 +1,31 @@
+"""
+continue和break
+"""
+
+# for i in range(1, 6):
+#     print('jujube1')
+#     continue
+#     print('yujube2')
+
+
+# for j in range(1, 6):
+#     print('语句3')
+#     for i in range(1, 6):
+#         print('jujube1')
+#         continue
+#         print('yujube2')
+#     print('语句4')
+
+# for i in range(100):
+#     print('1')
+#     break
+#     print('2')
+# print('3')
+
+for j in range(1, 2):
+    print('1')
+    for i in range(100):
+        print('2')
+        break
+        print('3')
+    print('4')
