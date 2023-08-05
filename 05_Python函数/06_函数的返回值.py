e564b441e4b35be48ca5029884893e8df286d28a
4399chengzi <981691590@qq.com>
2023-08-05 22:19:27 +0800

    函数的返回值

---
+++ 05_Python函数/06_函数的返回值.py
@@ -0,0 +1,19 @@
+"""
+函数的返回值
+"""
+
+
+# def add(x, y):
+#     result = x + y
+#     print(f"{x}+{y}={x + y}")
+#
+#
+# add(1, 1)
+
+def add(x, y, z):
+    result = x + y + z
+    return result
+    print('1')
+
+
+print(add(1, 1, 1))
