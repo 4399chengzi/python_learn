d4bf0bacbcc804b33979f5b19d3a408ff34f8245
4399chengzi <981691590@qq.com>
2023-08-05 19:33:51 +0800

    判断语句的嵌套

---
+++ 03_Python判断语句/08_判断语句的嵌套.py
@@ -0,0 +1,18 @@
+"""
+演示判断语句的嵌套使用
+"""
+
+if 2 > 1:
+    print(1)
+    if 3 > 2:
+        print(1)
+        if 4 > 3:
+            print(1)
+        elif 5 > 4:
+            print(1)
+        else:
+            print(1)
+    else:
+        print(1)
+else:
+    print(1)
