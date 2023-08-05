8c572d2904294f71940a81d8f47146aa255bf677
4399chengzi <981691590@qq.com>
2023-08-05 22:06:50 +0800

    函数的传入参数

---
+++ 05_Python函数/04_函数的传入参数.py
@@ -0,0 +1,18 @@
+"""
+函数的传入参数
+"""
+
+
+# def add(x, y):
+#     result = x + y
+#     print(f"{x}+{y}={x + y}")
+#
+#
+# add(1, 1)
+
+def add(x, y, z):
+    result = x + y + z
+    print(f"{x}+{y}+{z}={x + y + z}")
+
+
+add(1, 1, 1)
