08ac5e10974d6a3cb66049899244e673b2f1bde4
4399chengzi <981691590@qq.com>
2023-08-05 11:26:52 +0800

    数据输入（input）.py

---
+++ 02_Python入门语法/15_数据输入(input语句).py
@@ -0,0 +1,18 @@
+"""
+演示Python的input语句
+获取键盘的输入信息
+"""
+# print("请告诉我你是谁?")
+# name = input()
+# print(f"我知道了，你是{name}")
+
+# name = input("请告诉我你是谁?")
+# print(f"我知道了，你是{name}")
+
+# 输入数字类型
+# num = input("请告诉我你的密码？")
+# print(num, type(num))
+
+num = int(input("请告诉我你的密码？"))
+# num = int(num)
+print(num, type(num))
