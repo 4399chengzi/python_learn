4907e103b8bef20570175b34454044913ce47a07
4399chengzi <981691590@qq.com>
2023-08-05 23:16:12 +0800

    列表下标索引

---
+++ 06_Python数据容器/02_列表下标索引.py
@@ -0,0 +1,15 @@
+"""
+列表下标索引
+"""
+
+my_list = [[1, 2, 3], [4, 5, 6]]
+print(my_list)
+print(type(my_list))
+
+print(my_list[0])
+print(my_list[1])
+print(my_list[-1])
+print(my_list[-2])
+print(my_list[0][0])
+print(my_list[0][1])
+print(my_list[0][2])
