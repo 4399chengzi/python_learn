830c776ad4b01edc587ea649a13926a1bf7eb180
4399chengzi <981691590@qq.com>
2023-08-06 21:06:28 +0800

    序列和切片

---
+++ 06_Python数据容器/09_序列和切片.py
@@ -0,0 +1,18 @@
+"""
+演示序列和切片
+"""
+
+my_list = [0, 1, 2, 3, 4, 5, 6]
+
+print(my_list[1:4])
+
+my_tuple = (0, 1, 2, 3, 4, 5, 6)
+print(my_tuple[:])
+
+my_str = "01234567"
+print(my_str[::2])
+print(my_str[::-1])
+
+print(my_list[3:1:-1])
+
+print(my_tuple[::2])
