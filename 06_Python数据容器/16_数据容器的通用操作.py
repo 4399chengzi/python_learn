557deb8b093158216fad673b00ce24f92647e7b1
4399chengzi <981691590@qq.com>
2023-08-06 22:35:22 +0800

    数据容器的通用操作

---
+++ 06_Python数据容器/16_数据容器的通用操作.py
@@ -0,0 +1,22 @@
+"""
+演示数据容器的通用操作
+"""
+my_list = [3, 1, 2, 5, 4]
+# len()
+
+# max() min()
+
+# list()
+
+# tuple()
+
+# str()
+
+# set()
+print(set(my_list))
+# sorted(容器, reverse=True)
+
+
+print(sorted(my_list))
+print(sorted(my_list, reverse=True))
+print(my_list)
