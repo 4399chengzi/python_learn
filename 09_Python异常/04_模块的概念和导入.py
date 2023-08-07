18e47120d5949a543f259f83bb6eb8b2a680e71b
4399chengzi <981691590@qq.com>
2023-08-07 19:02:07 +0800

    模块的概念和导入

---
+++ 09_Python异常/04_模块的概念和导入.py
@@ -0,0 +1,33 @@
+"""
+演示模块的概念和导入
+"""
+
+# import time
+#
+# print('b')
+# time.sleep(1)
+# print('o')
+
+# from time import sleep
+#
+# print('b')
+# sleep(1)
+# print('o')
+
+# from time import *
+#
+# print('b')
+# sleep(1)
+# print('o')
+
+# import time as t
+#
+# print('b')
+# t.sleep(1)
+# print('o')
+
+from time import sleep as sl
+
+print('b')
+sl(1)
+print('o')
