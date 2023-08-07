161c998705c03dc8118b2bbfb8eef1bc9c3f406b
4399chengzi <981691590@qq.com>
2023-08-07 19:35:57 +0800

    自定义Python包

---
+++ 09_Python异常/06_自定义Python包.py
@@ -0,0 +1,19 @@
+"""
+演示自定义Python包
+"""
+
+# import package1.module1
+#
+# package1.module1.test(1, 2)
+
+# from package1 import module1
+#
+# module1.test(1, 2)
+
+# from package1.module1 import test
+#
+# test(1, 2)
+
+from package1 import *
+
+module1.test(1, 2)
---
+++ 09_Python异常/package1/module3.py
@@ -0,0 +1,12 @@
+"""
+演示
+"""
+__all__ = ['testA']
+
+
+def testA(a, b):
+    print(a + b)
+
+
+def testB(a, b):
+    print(a - b)
