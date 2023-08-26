fb1b7faa372c72f7cbaa71eabadd962591b716d0
4399chengzi <981691590@qq.com>
2023-08-26 15:55:22 +0800

    test

---
+++ 13_Python对象/01_test.py
@@ -0,0 +1,22 @@
+"""
+演示
+"""
+
+
+# 设计一个类
+class Student:
+    name = None
+    gender = None
+    nationality = None
+    native_place = None
+    age = None
+
+
+# 创建一个对象
+stu_1 = Student()
+
+# 对象属性进行赋值
+stu_1.name = '1'
+
+# 获取对象中记录的信息
+print(stu_1.name)
